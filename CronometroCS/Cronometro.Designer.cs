2b8dc4eb4aad71470035b0a943bb63d4f4d52e73
Thug-Okami <m.senna.cordeiro@gmail.com>
2025-01-10 16:15:58 -0300

    Função principal do projeto atribuida

--- CronometroCS/Cronometro.Designer.cs
+++ CronometroCS/Cronometro.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace CronometroCS
 {
     partial class FrmCronometro
@@ -40,46 +42,51 @@ namespace CronometroCS
             this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTempo.Location = new System.Drawing.Point(75, 27);
             this.lblTempo.Name = "lblTempo";
-            this.lblTempo.Size = new System.Drawing.Size(174, 55);
+            this.lblTempo.Size = new System.Drawing.Size(204, 37);
             this.lblTempo.TabIndex = 0;
-            this.lblTempo.Text = "Tempo";
+            this.lblTempo.Text = "00:00:00.000";
             // 
             // btnReiniciar
             // 
             this.btnReiniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReiniciar.Location = new System.Drawing.Point(12, 251);
+            this.btnReiniciar.Location = new System.Drawing.Point(12, 351);
             this.btnReiniciar.Name = "btnReiniciar";
             this.btnReiniciar.Size = new System.Drawing.Size(112, 98);
             this.btnReiniciar.TabIndex = 1;
             this.btnReiniciar.Text = "Reiniciar";
             this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
             // 
             // btnComecarPausar
             // 
             this.btnComecarPausar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnComecarPausar.Location = new System.Drawing.Point(210, 251);
+            this.btnComecarPausar.Location = new System.Drawing.Point(310, 351);
             this.btnComecarPausar.Name = "btnComecarPausar";
             this.btnComecarPausar.Size = new System.Drawing.Size(112, 98);
             this.btnComecarPausar.TabIndex = 2;
             this.btnComecarPausar.Text = "Iniciar / Pausar";
             this.btnComecarPausar.UseVisualStyleBackColor = true;
-            this.btnComecarPausar.Click += new System.EventHandler(this.btnComecarPausar_Click);
+            this.btnComecarPausar.Click += new System.EventHandler(this.btnComecarPausar_Click_1);
             // 
             // FrmCronometro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 361);
+            this.ClientSize = new System.Drawing.Size(434, 461);
             this.Controls.Add(this.btnComecarPausar);
             this.Controls.Add(this.btnReiniciar);
             this.Controls.Add(this.lblTempo);
             this.MaximizeBox = false;
             this.Name = "FrmCronometro";
-            this.Text = "Cronometro";
-            this.Load += new System.EventHandler(this.FrmCronometro_Load);
+            this.Text = "Cronômetro";
             this.ResumeLayout(false);
             this.PerformLayout();
 
+        }
+
+        private void btnComecarPausar_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
--- CronometroCS/Cronometro.cs
+++ CronometroCS/Cronometro.cs
@@ -14,25 +14,55 @@ namespace CronometroCS
 {
     public partial class FrmCronometro : Form
     {
+
+        private System.Windows.Forms.Timer timer;
+        private Stopwatch stopWatch;
+
         public FrmCronometro()
         {
             InitializeComponent();
+            stopWatch = new Stopwatch();
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1;
+            timer.Tick += new EventHandler(UpdateLabel);
         }
 
-        public class Cronometro
+        //Inicia e pausa a contagem do cronômetro
+        private void btnComecarPausar_Click_1(object sender, EventArgs e)
         {
-            Stopwatch stopwatch = new Stopwatch();
+            if (stopWatch.IsRunning)
+            {
+                stopWatch.Stop();
+                timer.Stop();
+            }
+            else
+            {
+                stopWatch.Start();
+                timer.Start();
+            }
         }
 
-        private void FrmCronometro_Load(object sender, EventArgs e)
-        {
 
+        private void UpdateLabel(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Formatação e visualização do valor de TimeSpan.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 1); 
+
+            lblTempo.Text = elapsedTime;
         }
 
-        private void btnComecarPausar_Click(object sender, EventArgs e)
+        //Reseta o valor do cronômetro e zera a contagem
+        private void btnReiniciar_Click(object sender, EventArgs e)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            Stopwatch.Start();
+            stopWatch.Stop();
+            timer.Stop();
+            stopWatch.Reset();
+
+            lblTempo.Text = "00:00:00.000";
         }
     }
 }
